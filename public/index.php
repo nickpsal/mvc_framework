972681c40ad5816bdb40ff075daa811e4641a5b8
nickpsal <nickpsal@gmail.com>
2023-01-03 11:57:19 +0200

    bug fix

--- public/index.php
+++ public/index.php
@@ -7,6 +7,6 @@
     /**error display */
     DEBUG ? ini_set("display_errors", 1) : ini_set("display_errors", 0);
     /** check php version req **/
-    check_extensions();
     check_min_php_version();
+    check_extensions();
     $app = new App;
